e52fd24c4113f03101fa868b06b252eb6dded1a0
viviana2397 <vivianaguerracustodio@gmail.com>
2020-03-23 10:12:12 -0500

    probando sort

--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
 /* interacción con el DOM event listeners o event handlers, se usan las que estàn en data.js */
 import {
-  allSelection,
+  allSelection, orderAZ,
 } from './data.js';
 import lol from './data/lol/lol.js';
 
 const allChampionList = lol.data;
-// console.log(allchampionList);
 
-// console.log(root);
 const divContador = document.getElementById('contador');
 const buttonAllChampions = document.getElementById('button_colection');
 
 buttonAllChampions.addEventListener('click', () => {
   const list = document.querySelector('#root');
   list.innerHTML = ''; /* Primero lo vacía luego coloca todo a todos los campeones */
-  Object.values(allChampionList).forEach((champion) => {
+  // eslint-disable-next-line no-unused-vars
+  const seeAllChampion = Object.values(allChampionList).forEach((champion) => {
     const div = document.createElement('div');
     const img = document.createElement('img');
     const p = document.createElement('p');
@@ -30,20 +29,13 @@ buttonAllChampions.addEventListener('click', () => {
 });
 
 const liRoles = document.querySelectorAll('.Roles');
-// console.log(typeof liRoles); /*is an object*/
 liRoles.forEach((option) => {
   option.addEventListener('click', () => {
-    // const functionRoles = () => {
     const typeRol = option.getAttribute('data-value');
-    // console.log(typeRol);
     const functionFilterRol = allSelection(allChampionList, typeRol);
-    // console.log(functionFilterRol);
     const list = document.querySelector('#root');
     list.innerHTML = '';
-    /* Llamamos otra vez al div que contiene list,la limpiamos=> pinta lo nuevo que vamos a darle */
-    // console.log(root);
     Object.values(functionFilterRol).forEach((champion) => {
-      /* se busca el valor=>volvemos usar object.values y acceder a los valores de lo importado */
       const div = document.createElement('div');
       const img = document.createElement('img');
       const p = document.createElement('p');
@@ -61,4 +53,23 @@ liRoles.forEach((option) => {
     });
     divContador.innerHTML = (`${typeRol} ${functionFilterRol.length}`);
   });
+});
+
+document.getElementById('AtoZ').addEventListener('click', () => {
+  const list = document.querySelector('#root');
+  list.innerHTML = '';
+  const seeAllChampion = Object.values(allChampionList).forEach((champion) => {
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    p.className = 'nameOfChampion';
+    img.className = 'imageOfChampion';
+    p.innerHTML = `${champion.name}`;
+    img.src = `${champion.splash}`;
+    div.appendChild(img);
+    div.appendChild(p);
+    list.appendChild(div);
+    divContador.innerHTML = 'Todos los campeones 122';
+  });
+  return seeAllChampion(orderAZ(allChampionList));
 });
