fe9fa6d9229cdf2d788e5a0275c2eaeb8c369e7c
viviana2397 <vivianaguerracustodio@gmail.com>
2020-03-25 15:52:50 -0500

    ...

--- src/data.js
+++ src/data.js
@@ -23,10 +23,9 @@ export const sortOrder = (arr, condition) => {
     arr.sort((a, b) => ((a.id < b.id) ? -1 : 1));
   }
   return arr;
-
-
-export const filterBySkill = (allchampionList, typeSkill) => {
-  const arrayValuesChampions = Object.values(allchampionList);
-  return arrayValuesChampions.filter(championsList => championsList.info.typeSkill.indexOf(typeSkill) >= 0);
-
 };
+
+// export const filterBySkill = (allchampionList, typeSkill) => {
+//  const arrayValuesChampions = Object.values(allchampionList);
+//  return arrayValuesChampions.filter(championsList =>
+// championsList.info.typeSkill.indexOf(typeSkill) >= 0);};
